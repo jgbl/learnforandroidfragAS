c9420beb9ea17a82eed2049427b0c145a4b1d52e
jhmgbl <jhmgbl@gmail.com>
2015-10-10 03:06:31 +0200

    added quizlet import

---
+++ learnforAndroidFrag/src/main/java/br/com/thinkti/android/filechooserfrag/RowData.java
@@ -0,0 +1,12 @@
+package br.com.thinkti.android.filechooserfrag;
+
+/**
+ * Created by hmnatalie on 09.10.15.
+ */
+public class RowData  {
+    int id;
+    String name;
+    String description;
+    int numCards ;
+    String lastModified ;
+}
