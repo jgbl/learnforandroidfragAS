b93ce6a338df1bb7bd41ea1021992437a6d6c27c
jhmgbl <jhmgbl@gmail.com>
2016-12-21 21:30:52 +0100

    always loading pictures in reverse mode

--- learnforAndroidFrag/src/main/java/org/de/jmg/learn/_MainActivity.java
+++ learnforAndroidFrag/src/main/java/org/de/jmg/learn/_MainActivity.java
@@ -1070,7 +1070,7 @@ public class _MainActivity extends Fragment implements RemoveCallbackListener {
 						}
 					};
 					tspanKom.setSpan(QuizletPicture, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-					if (!(showBeds || _vok.reverse) && _vok.getShowPics())
+					if ((!showBeds || _vok.reverse) && _vok.getShowPics())
 					{
 						QuizletPicture.onClick(null);
 					}
